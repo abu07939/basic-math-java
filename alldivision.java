be7cb074dfdf192c632276d6c2c2c84e239cfaf3
abu07939 <abufaizil407@gmail.com>
2026-02-17 19:33:09 +0530

    to count all division

---
+++ alldivision.java
@@ -0,0 +1,14 @@
+public class alldivision {
+    public static void main(String[] args) {
+        int n = 97;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                System.out.println(i);
+
+
+            }
+        }
+    }
+
+    
+}
